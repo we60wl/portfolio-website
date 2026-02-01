eb31a1651f7582c17b99462184808a4935ba46c9
webowl <naxi03@mail.ru>
2026-02-02 00:28:23 +0300

    updated image path

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,12 +3,10 @@ import typescriptLogo from '../assets/typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-const { BASE_URL } = import.meta.env;
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${BASE_URL}${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
